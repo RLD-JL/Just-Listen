6000e664c7621cd34bef40dd80d49d3e135f299c
Laurentiu <Laurentiu@DESKTOP-DU0RLR6>
2021-09-19 20:00:58 +0300

    Added PlayerBottomBar, needs info + to show only music is playing

--- androidApp/src/main/java/com/example/audius/android/ui/bottombars/Level1BottomBar.kt
+++ androidApp/src/main/java/com/example/audius/android/ui/bottombars/Level1BottomBar.kt
@@ -1,11 +1,15 @@
 package com.example.audius.android.ui.bottombars
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.audius.Navigation
 import com.example.audius.ScreenIdentifier
@@ -15,18 +19,23 @@ import com.example.audius.viewmodel.screens.Level1Navigation
 fun Navigation.Level1BottomBar(
     selectedTab: ScreenIdentifier
 ) {
-    BottomNavigation(content = {
-        BottomNavigationItem(
-            icon = { Icon(Icons.Default.Menu, "ALL") },
-            label = { Text("Playlist", fontSize = 13.sp) },
-            selected = selectedTab.URI == Level1Navigation.Playlist.screenIdentifier.URI,
-            onClick = { navigateByLevel1Menu(Level1Navigation.Playlist) }
-        )
-        BottomNavigationItem(
-            icon = { Icon(Icons.Default.Star, "FAVORITES") },
-            label = { Text("Favourites", fontSize = 13.sp) },
-            selected = selectedTab.URI == Level1Navigation.AllTrending.screenIdentifier.URI,
-            onClick = { navigateByLevel1Menu(Level1Navigation.AllTrending) }
-        )
-    })
+    Box(modifier = Modifier.fillMaxWidth()) {
+        PlayerBottomBar(modifier = Modifier, songIcon = "", title ="yolooo" ) {
+        }
+
+        BottomNavigation(modifier = Modifier.align(Alignment.BottomCenter), content = {
+            BottomNavigationItem(
+                icon = { Icon(Icons.Default.Menu, "ALL") },
+                label = { Text("Playlist", fontSize = 13.sp) },
+                selected = selectedTab.URI == Level1Navigation.Playlist.screenIdentifier.URI,
+                onClick = { navigateByLevel1Menu(Level1Navigation.Playlist) }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Default.Star, "FAVORITES") },
+                label = { Text("Favourites", fontSize = 13.sp) },
+                selected = selectedTab.URI == Level1Navigation.AllTrending.screenIdentifier.URI,
+                onClick = { navigateByLevel1Menu(Level1Navigation.AllTrending) }
+            )
+        })
+    }
 }
