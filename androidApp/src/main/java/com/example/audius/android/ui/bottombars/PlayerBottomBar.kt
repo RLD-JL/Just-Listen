61801ba82bb8b2cc7104e5760bba44f67781ae31
Laurentiu <Laurentiu@DESKTOP-DU0RLR6>
2021-09-25 17:39:50 +0300

    Added loading for switching music

--- androidApp/src/main/java/com/example/audius/android/ui/bottombars/PlayerBottomBar.kt
+++ androidApp/src/main/java/com/example/audius/android/ui/bottombars/PlayerBottomBar.kt
@@ -1,26 +1,17 @@
 package com.example.audius.android.ui.bottombars
 
 import android.media.session.PlaybackState
-import android.support.v4.media.MediaMetadataCompat
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SnackbarDefaults
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Place
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -28,9 +19,6 @@ import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import com.example.audius.android.R
 import com.example.audius.android.exoplayer.MusicServiceConnection
-import com.example.audius.android.exoplayer.isPlayEnabled
-import com.example.audius.android.exoplayer.isPlaying
-import com.example.audius.android.exoplayer.isPrepared
 
 @Composable
 fun PlayerBottomBar(
@@ -65,21 +53,30 @@ fun PlayerBottomBar(
             imageVector = Icons.Default.FavoriteBorder, modifier = Modifier.padding(8.dp),
             contentDescription = null
         )
-        if (musicServiceConnection.playbackState.value?.state != PlaybackState.STATE_PLAYING) {
+        if (musicServiceConnection.playbackState.value?.state != PlaybackState.STATE_PLAYING &&
+            musicServiceConnection.playbackState.value?.state != PlaybackState.STATE_BUFFERING) {
             Icon(
-                painter = painterResource(id = R.drawable.exo_icon_play), modifier = Modifier.padding(8.dp).clickable(
-                    onClick = {musicServiceConnection.transportControls.play()}
-                ),
-                contentDescription = null
-            )
-        } else {
-            Icon(
-                painter = painterResource(id = R.drawable.exo_icon_pause), modifier = Modifier.padding(8.dp).clickable(
-                    onClick = {musicServiceConnection.transportControls.pause()}
-                ),
+                painter = painterResource(id = R.drawable.exo_icon_play), modifier = Modifier
+                    .padding(8.dp)
+                    .clickable(
+                        onClick = { musicServiceConnection.transportControls.play() }
+                    ),
                 contentDescription = null
             )
         }
+        if (musicServiceConnection.playbackState.value?.state == PlaybackState.STATE_PLAYING)
+        {
+            Icon(
+                painter = painterResource(id = R.drawable.exo_icon_pause), modifier = Modifier
+                    .padding(8.dp)
+                    .clickable(
+                        onClick = { musicServiceConnection.transportControls.pause() }
+                    ),
+                contentDescription = null
+            )
+        }
+
+            isLoading(musicServiceConnection.playbackState.value?.state == PlaybackState.STATE_BUFFERING)
 
         Icon(
             painter = painterResource(id = R.drawable.exo_ic_skip_next),
@@ -91,20 +88,19 @@ fun PlayerBottomBar(
     }
 }
 
-fun play(musicServiceConnection: MusicServiceConnection, mediaId: String) {
-    val isPrepared = musicServiceConnection.playbackState.value?.isPrepared ?: false
-    if (isPrepared && mediaId ==
-        musicServiceConnection.currentPlayingSong.value?.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID)
-    ) {
-        musicServiceConnection.playbackState.value?.let { playbackState ->
-            when {
-                playbackState.isPlaying -> musicServiceConnection.transportControls.pause()
-                playbackState.isPlayEnabled -> musicServiceConnection.transportControls.play()
-                else -> Unit
-            }
+@Composable
+fun isLoading(isLoading: Boolean) {
+    if (isLoading) {
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size(24.dp*2f)
+         ) {
+            CircularProgressIndicator(color = Color.Black)
+            Icon(
+                painter = painterResource(id = R.drawable.exo_icon_pause), modifier = Modifier,
+                contentDescription = null
+            )
         }
-    } else {
-        musicServiceConnection.transportControls.playFromMediaId(mediaId, null)
     }
 }
-
