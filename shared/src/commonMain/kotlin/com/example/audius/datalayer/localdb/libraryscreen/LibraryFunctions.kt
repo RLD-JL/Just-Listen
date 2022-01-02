4785912ac9e188348739a3b18b3e4edaff19bada
laurentiu <laurentzyu_danyel@yahoo.com>
2022-01-02 10:52:00 +0200

    Fix mismatched between favorite and recent

--- shared/src/commonMain/kotlin/com/example/audius/datalayer/localdb/libraryscreen/LibraryFunctions.kt
+++ shared/src/commonMain/kotlin/com/example/audius/datalayer/localdb/libraryscreen/LibraryFunctions.kt
@@ -15,7 +15,7 @@ fun LocalDb.saveSongToFavorites(
 }
 
 fun LocalDb.getFavoritePlaylist(): List<PlayListModel> {
-    return libraryQueries.getFavoritePlaylist(mapper = { id, title, user, songImgList, _, _ ->
+    return libraryQueries.getFavoritePlaylist(mapper = { id, title, user, songImgList, _, _, _ ->
         PlayListModel(id = id, playlistTitle = title, title = title, user = user, songImgList = songImgList)
     }).executeAsList()
 }
@@ -30,7 +30,7 @@ fun LocalDb.saveSongRecentSongs(
 }
 
 fun LocalDb.getRecentPlayed(): List<PlayListModel> {
-    return libraryQueries.getRecentPlayed(mapper = { id, title, user, songImgList, _, _ ->
+    return libraryQueries.getRecentPlayed(mapper = { id, title, user, songImgList, _, _, _ ->
         PlayListModel(id = id, playlistTitle = title, title = title, user = user, songImgList = songImgList)
     }).executeAsList()
 }
