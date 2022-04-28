97316367356f3d4090f2f4e223abd57f3ee74bae
laurentiu <laurentzyu_danyel@yahoo.com>
2022-04-28 14:23:08 +0300

    Playlist gets updated after adding it

--- androidApp/src/main/java/com/example/audius/android/ui/screenpicker/ScreenPicker.kt
+++ androidApp/src/main/java/com/example/audius/android/ui/screenpicker/ScreenPicker.kt
@@ -20,6 +20,7 @@ import com.example.audius.datalayer.models.UserModel
 import com.example.audius.viewmodel.screens.Screen.*
 import com.example.audius.viewmodel.screens.addplaylist.AddPlaylistParams
 import com.example.audius.viewmodel.screens.addplaylist.addPlaylist
+import com.example.audius.viewmodel.screens.addplaylist.updatePlaylist
 import com.example.audius.viewmodel.screens.library.saveSongToFavorites
 import com.example.audius.viewmodel.screens.library.saveSongToRecent
 import com.example.audius.viewmodel.screens.playlistdetail.PlaylistDetailParams
@@ -158,7 +159,9 @@ fun Navigation.ScreenPicker(
         )
         AddPlaylist -> AddPlaylistScreen(addPlaylistState = stateProvider.get(screenIdentifier),
         onAddPlaylistClicked = {playlistName, playlistDescription ->
-        events.addPlaylist(playlistName, playlistDescription)})
+        events.addPlaylist(playlistName, playlistDescription)
+        events.updatePlaylist()}
+        )
     }
 }
 
--- shared/src/commonMain/kotlin/com/example/audius/viewmodel/screens/addplaylist/AddPlaylistEvents.kt
+++ shared/src/commonMain/kotlin/com/example/audius/viewmodel/screens/addplaylist/AddPlaylistEvents.kt
@@ -1,9 +1,19 @@
 package com.example.audius.viewmodel.screens.addplaylist
 
+import com.example.audius.datalayer.datacalls.addplaylistscreen.getAddPlaylist
 import com.example.audius.datalayer.datacalls.addplaylistscreen.savePlaylist
 import com.example.audius.viewmodel.Events
+import com.example.audius.viewmodel.screens.search.SearchScreenState
 
 fun Events.addPlaylist(
     playlistName: String,
     playlistDescription: String?
 ) = dataRepository.savePlaylist(playlistName, playlistDescription)
+
+fun Events.updatePlaylist() = screenCoroutine {
+    val playlist = dataRepository.getAddPlaylist()
+    stateManager.updateScreen(AddPlaylistState::class) {
+        it.copy(playlistsCreated = playlist)
+
+    }
+}
