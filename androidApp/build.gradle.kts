8ebef6e10c117a9202ba3c17c56f47b57470379d
laurentiu <laurentzyu_danyel@yahoo.com>
2022-05-11 17:19:10 +0300

    update for google play store

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "com.rld.justlisten.android"
         minSdk = 21
         targetSdk = 30
-        versionCode = 7
-        versionName = "0.1.9"
+        versionCode = 8
+        versionName = "0.2.0"
         vectorDrawables {
             useSupportLibrary = true
         }
