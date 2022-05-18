d15fb81966690989bda850b096b60854a2826198
laurentiu <laurentzyu_danyel@yahoo.com>
2022-05-18 20:32:06 +0300

    More categories added

--- shared/src/commonMain/kotlin/com/rld/justlisten/viewmodel/screens/playlist/TracksCategory.kt
+++ shared/src/commonMain/kotlin/com/rld/justlisten/viewmodel/screens/playlist/TracksCategory.kt
@@ -4,7 +4,17 @@ enum class TracksCategory(val value: String) {
     ELECTRONIC("Electronic"),
     ROCK("Rock"),
     RAP("Rap"),
-    ALTERNATIVE("Alternative")
+    ALTERNATIVE("Alternative"),
+    EXPERIMENTAL("Experimental"),
+    PUNK("Punk"),
+    POP("Pop"),
+    FOLK("Folk"),
+    AMBIENT("Ambient"),
+    JAZZ("Jazz"),
+    CLASSICAL("Classical"),
+    COUNTRY("Country"),
+    KIDS("Kids"),
+    AUDIOBOOKS("Audiobooks"),
 }
 
 enum class TimeRange(val value: String) {
@@ -14,9 +24,24 @@ enum class TimeRange(val value: String) {
 }
 
 fun getTrackCategory(): List<TracksCategory> {
-    return listOf(TracksCategory.ELECTRONIC, TracksCategory.ROCK, TracksCategory.RAP, TracksCategory.ALTERNATIVE)
+    return listOf(
+        TracksCategory.ELECTRONIC,
+        TracksCategory.ROCK,
+        TracksCategory.RAP,
+        TracksCategory.ALTERNATIVE,
+        TracksCategory.EXPERIMENTAL,
+        TracksCategory.PUNK,
+        TracksCategory.POP,
+        TracksCategory.FOLK,
+        TracksCategory.AMBIENT,
+        TracksCategory.JAZZ,
+        TracksCategory.CLASSICAL,
+        TracksCategory.COUNTRY,
+        TracksCategory.KIDS,
+        TracksCategory.AUDIOBOOKS
+    )
 }
 
 fun getTimeRange(): List<TimeRange> {
-    return listOf(TimeRange.WEEK,TimeRange.MONTH,TimeRange.ALLTIME)
+    return listOf(TimeRange.WEEK, TimeRange.MONTH, TimeRange.ALLTIME)
 }
