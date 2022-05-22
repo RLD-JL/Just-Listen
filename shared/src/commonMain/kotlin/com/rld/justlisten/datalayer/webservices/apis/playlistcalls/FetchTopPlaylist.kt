675e97498c7261b27950ba437cc870d2f5cc0658
laurentiu <laurentzyu_danyel@yahoo.com>
2022-05-22 15:36:43 +0300

    go live version + target sdk 31

--- shared/src/commonMain/kotlin/com/rld/justlisten/datalayer/webservices/apis/playlistcalls/FetchTopPlaylist.kt
+++ shared/src/commonMain/kotlin/com/rld/justlisten/datalayer/webservices/apis/playlistcalls/FetchTopPlaylist.kt
@@ -1,6 +1,7 @@
 package com.rld.justlisten.datalayer.webservices.apis.playlistcalls
 
 import com.rld.justlisten.datalayer.models.PlayListModel
+import com.rld.justlisten.datalayer.utils.Constants.appName
 import com.rld.justlisten.datalayer.webservices.ApiClient
 import com.rld.justlisten.viewmodel.screens.playlist.PlayListEnum
 import com.rld.justlisten.viewmodel.screens.playlist.PlayListEnum.*
@@ -9,17 +10,17 @@ import kotlinx.serialization.Serializable
 
 suspend fun ApiClient.fetchPlaylist(index: Int, playListEnum: PlayListEnum, playlistId: String="", queryPlaylist: String = "Rock"): PlayListResponse? {
    return when (playListEnum) {
-       TOP_PLAYLIST -> getResponse("/playlists/top?type=playlist&limit=${index}&app_name=ExampleApp")
-       REMIX -> getResponse("/playlists/search?query=${queryPlaylist}&limit=${index}&app_name=ExampleApp")
-       CURRENT_PLAYLIST -> getResponse("/playlists/${playlistId}/tracks?app_name=ExampleApp")
-       HOT -> getResponse("/playlists/search?query=${queryPlaylist}&limit=${index}&app_name=ExampleApp")
+       TOP_PLAYLIST -> getResponse("/playlists/top?type=playlist&limit=${index}&app_name=$appName")
+       REMIX -> getResponse("/playlists/search?query=${queryPlaylist}&limit=${index}&app_name=$appName")
+       CURRENT_PLAYLIST -> getResponse("/playlists/${playlistId}/tracks?app_name=$appName")
+       HOT -> getResponse("/playlists/search?query=${queryPlaylist}&limit=${index}&app_name=$appName")
        FAVORITE -> TODO()
        CREATED_BY_USER -> TODO()
    }
 }
 
 suspend fun ApiClient.getTracks(limit: Int, category: String, timeRange: String) : PlayListResponse? {
-    return getResponse("/full/tracks/trending/EJ57D?genre=${category}&limit=${limit}&time=${timeRange}&app_name=ExampleApp")
+    return getResponse("/full/tracks/trending/EJ57D?genre=${category}&limit=${limit}&time=${timeRange}&app_name=$appName")
 }
 
 @Serializable
--- shared/src/commonMain/kotlin/com/rld/justlisten/datalayer/webservices/apis/searchcalls/SearchScreenCalls.kt
+++ shared/src/commonMain/kotlin/com/rld/justlisten/datalayer/webservices/apis/searchcalls/SearchScreenCalls.kt
@@ -1,5 +1,6 @@
 package com.rld.justlisten.datalayer.webservices.apis.searchcalls
 
+import com.rld.justlisten.datalayer.utils.Constants
 import com.rld.justlisten.datalayer.webservices.ApiClient
 import com.rld.justlisten.datalayer.webservices.apis.playlistcalls.PlayListResponse
 import com.rld.justlisten.viewmodel.screens.search.SearchEnum
@@ -7,7 +8,7 @@ import com.rld.justlisten.viewmodel.screens.search.SearchEnum.*
 
 suspend fun ApiClient.searchFor(searchFor: String, searchEnum: SearchEnum): PlayListResponse? {
     return when(searchEnum) {
-        TRACKS -> getResponse("/tracks/search?query=${searchFor}&app_name=EXAMPLEAPP")
-        PLAYLIST -> getResponse("/playlists/search?query=${searchFor}&app_name=EXAMPLEAPP")
+        TRACKS -> getResponse("/tracks/search?query=${searchFor}&app_name=${Constants.appName}")
+        PLAYLIST -> getResponse("/playlists/search?query=${searchFor}&app_name=${Constants.appName}")
     }
 }
