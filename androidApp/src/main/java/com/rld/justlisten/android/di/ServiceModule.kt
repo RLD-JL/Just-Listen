98a3b05f1a117c7a1d89c6c6f499910a5ae48a15
laurentiu <laurentzyu_danyel@yahoo.com>
2022-05-26 19:02:41 +0300

    Shrinking purpose

--- androidApp/src/main/java/com/rld/justlisten/android/di/ServiceModule.kt
+++ androidApp/src/main/java/com/rld/justlisten/android/di/ServiceModule.kt
@@ -3,13 +3,17 @@ package com.rld.justlisten.android.di
 import android.content.Context
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.RenderersFactory
 import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.audio.MediaCodecAudioRenderer
+import com.google.android.exoplayer2.mediacodec.MediaCodecSelector
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
+
 
 @Module
 @InstallIn(ServiceComponent::class)
@@ -21,7 +25,8 @@ object ServiceModule {
     fun provideExoPlayer(
         @ApplicationContext context: Context,
         audioAttributes: AudioAttributes,
-    ) = ExoPlayer.Builder(context).build().apply {
+        renderersFactory: RenderersFactory
+    ) = ExoPlayer.Builder(context, renderersFactory).build().apply {
         setAudioAttributes(audioAttributes, true)
         setHandleAudioBecomingNoisy(true)
     }
@@ -32,4 +37,15 @@ object ServiceModule {
         .setContentType(C.CONTENT_TYPE_MUSIC)
         .setUsage(C.USAGE_MEDIA)
         .build()
+
+    @Provides
+    @ServiceScoped
+    fun provideRenderFactory(@ApplicationContext context: Context) =
+        RenderersFactory { handler, _, audioListener, _, _ ->
+            arrayOf(
+                MediaCodecAudioRenderer(
+                    context, MediaCodecSelector.DEFAULT, handler, audioListener
+                )
+            )
+        }
 }
