8e8f9ea96be199ea9322c175f0ab2a4b8d952498
laurentiu <laurentzyu_danyel@yahoo.com>
2022-05-26 20:47:50 +0300

    Caching to save data

--- androidApp/src/main/java/com/rld/justlisten/android/di/ServiceModule.kt
+++ androidApp/src/main/java/com/rld/justlisten/android/di/ServiceModule.kt
@@ -6,13 +6,22 @@ import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.RenderersFactory
 import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.audio.MediaCodecAudioRenderer
+import com.google.android.exoplayer2.database.DatabaseProvider
+import com.google.android.exoplayer2.database.StandaloneDatabaseProvider
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.upstream.HttpDataSource
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource
+import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor
+import com.google.android.exoplayer2.upstream.cache.SimpleCache
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
+import java.io.File
 
 
 @Module
@@ -25,8 +34,10 @@ object ServiceModule {
     fun provideExoPlayer(
         @ApplicationContext context: Context,
         audioAttributes: AudioAttributes,
-        renderersFactory: RenderersFactory
-    ) = ExoPlayer.Builder(context, renderersFactory).build().apply {
+        renderersFactory: RenderersFactory,
+        cacheDataSourceFactory: CacheDataSource.Factory
+    ) = ExoPlayer.Builder(context, renderersFactory)
+        .setMediaSourceFactory(DefaultMediaSourceFactory(cacheDataSourceFactory)).build().apply {
         setAudioAttributes(audioAttributes, true)
         setHandleAudioBecomingNoisy(true)
     }
@@ -48,4 +59,20 @@ object ServiceModule {
                 )
             )
         }
+
+    @Provides
+    @ServiceScoped
+    fun provideCacheFactory(@ApplicationContext context: Context): CacheDataSource.Factory {
+
+        val httpDataSourceFactory: HttpDataSource.Factory =
+            DefaultHttpDataSource.Factory()
+
+        val evictor = LeastRecentlyUsedCacheEvictor((100 * 1024 * 1024).toLong())
+        val databaseProvider: DatabaseProvider = StandaloneDatabaseProvider(context)
+
+        val simpleCache = SimpleCache(File(context.cacheDir, "media"), evictor, databaseProvider)
+
+        return CacheDataSource.Factory().setCache(simpleCache)
+            .setUpstreamDataSourceFactory(httpDataSourceFactory)
+    }
 }
