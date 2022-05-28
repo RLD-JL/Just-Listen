949943176a92eb073c30975c6af0a474f5cdab88
laurentiu <laurentzyu_danyel@yahoo.com>
2022-05-28 10:36:34 +0300

    Added shimmer effect for playlists

---
+++ androidApp/src/main/java/com/rld/justlisten/android/ui/components/AnimatedShimmer.kt
@@ -0,0 +1,53 @@
+package com.rld.justlisten.android.ui.components
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun AnimatedShimmer(width: Dp, height: Dp) {
+    val shimmerColors = listOf(
+        MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+        MaterialTheme.colors.onSurface.copy(alpha = 0.2f),
+        MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+    )
+
+    val transition = rememberInfiniteTransition()
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
+    ShimmerGridItem(brush = brush, width, height)
+}
+
+@Composable
+fun ShimmerGridItem(brush: Brush, width: Dp, height: Dp) {
+    Box(
+        modifier = Modifier
+            .width(width)
+            .height(height)
+            .background(brush)
+    )
+}
