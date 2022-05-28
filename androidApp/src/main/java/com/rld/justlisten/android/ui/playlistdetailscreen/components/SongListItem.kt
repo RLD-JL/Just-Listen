45d517bf85fcf9bf051d3da5266ff5e29f5e833d
laurentiu <laurentzyu_danyel@yahoo.com>
2022-05-28 11:01:10 +0300

    Added shimmer effect for songs list

--- androidApp/src/main/java/com/rld/justlisten/android/ui/playlistdetailscreen/components/SongListItem.kt
+++ androidApp/src/main/java/com/rld/justlisten/android/ui/playlistdetailscreen/components/SongListItem.kt
@@ -1,35 +1,37 @@
 package com.rld.justlisten.android.ui.playlistdetailscreen.components
 
-import android.graphics.drawable.ColorDrawable
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberAsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.request.CachePolicy
 import coil.request.ImageRequest
+import com.rld.justlisten.android.ui.components.AnimatedShimmer
 import com.rld.justlisten.datalayer.models.SongIconList
 import com.rld.justlisten.datalayer.models.UserModel
 import com.rld.justlisten.viewmodel.screens.playlist.PlaylistItem
 
-@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun SongListItem(
     playlistItem: PlaylistItem, onSongClicked: (String) -> Unit,
@@ -45,19 +47,19 @@ fun SongListItem(
             ),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        val painter = rememberAsyncImagePainter(
-            model = ImageRequest.Builder(context = LocalContext.current)
-                .placeholder(ColorDrawable(MaterialTheme.colors.secondaryVariant.toArgb()))
-                .data(playlistItem.songIconList.songImageURL150px).allowHardware(false).build()
-        )
-
-        Image(
-            painter = painter,
+        SubcomposeAsyncImage(model = ImageRequest.Builder(LocalContext.current)
+            .data(playlistItem.songIconList.songImageURL150px)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .build(),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(55.dp)
-                .padding(4.dp)
+                .padding(4.dp),
+            loading = {
+                AnimatedShimmer(width = 55.dp, height = 55.dp)
+            }
         )
         Column(
             modifier = Modifier
