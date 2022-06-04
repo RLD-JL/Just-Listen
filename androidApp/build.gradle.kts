e332b9deaea510740299399ae78edf27751e7578
laurentiu <laurentzyu_danyel@yahoo.com>
2022-06-04 08:58:29 +0300

    update to latest compose

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,12 +16,12 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.4.0")
     implementation("androidx.palette:palette-ktx:1.0.0")
 
-    implementation("androidx.compose.ui:ui-util:1.2.0-beta02")
+    implementation("androidx.compose.ui:ui-util:1.2.0-beta03")
+    implementation("androidx.compose.material:material:1.2.0-beta03")
+    implementation("androidx.compose.ui:ui:1.2.0-beta03")
     implementation("androidx.compose.animation:animation:1.1.1")
-    implementation("androidx.compose.ui:ui:1.2.0-beta02")
     debugImplementation("androidx.compose.ui:ui-tooling:1.1.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
-    implementation("androidx.compose.material:material:1.2.0-beta02")
 
     implementation("com.google.android.exoplayer:exoplayer:2.17.1")
     implementation("com.google.android.exoplayer:extension-mediasession:2.17.1")
@@ -50,11 +50,11 @@ kapt {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = 32
     defaultConfig {
         applicationId = "com.rld.justlisten.android"
         minSdk = 21
-        targetSdk = 31
+        targetSdk = 32
         versionCode = 14
         versionName = "1.0.2"
         vectorDrawables {
