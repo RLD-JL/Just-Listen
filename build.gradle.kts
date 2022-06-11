e42ae5cb3a9193e428d749bfa8ebb56cdcc34afa
laurentiu <laurentzyu_danyel@yahoo.com>
2022-06-11 12:11:58 +0300

    kotlin and library update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 buildscript {
-    val compose_version by extra("1.2.0-alpha08")
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
 
-    val kotlin_version = "1.6.20"
+    val kotlin_version = "1.6.21"
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
 
@@ -27,3 +26,10 @@ allprojects {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+/*
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>{
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xuse-k2")
+    }
+}*/
