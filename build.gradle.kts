453c33b7ebc8723eb8edcd8c52f63e2280f31ed7
RLD-JL <106017010+rld-jl@users.noreply.github.com>
2022-07-23 15:48:10 +0300

    upgraded gradle version and added gradle version catalogs to manage dependencies easier

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,11 @@ buildscript {
         mavenCentral()
     }
 
-    val kotlin_version = "1.6.21"
-
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlinVersion.get()}")
         classpath("com.android.tools.build:gradle:7.2.1")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.3")
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
-
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.42")
     }
 }
 
@@ -27,9 +24,9 @@ tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
 
-/*
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>{
     kotlinOptions {
         freeCompilerArgs = listOf("-Xuse-k2")
     }
-}*/
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,59 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            version("kotlinVersion", "1.7.0")
+
+            version("ktorVersion", "2.0.2")
+            library("ktor-client-content-negotiation", "io.ktor", "ktor-client-content-negotiation").versionRef("ktorVersion")
+            library("ktor-client-core", "io.ktor", "ktor-client-core").versionRef("ktorVersion")
+            library("ktor-serialization-kotlinx-json", "io.ktor", "ktor-serialization-kotlinx-json").versionRef("ktorVersion")
+            library("ktor-client-logging", "io.ktor", "ktor-client-logging").versionRef("ktorVersion")
+            bundle(
+                "ktor",
+                listOf(
+                    "ktor-client-core",
+                    "ktor-client-content-negotiation",
+                    "ktor-serialization-kotlinx-json",
+                    "ktor-client-logging"
+                )
+            )
+
+            version("composeVersion", "1.3.0-alpha01")
+            library("compose-ui-util", "androidx.compose.ui", "ui-util").versionRef("composeVersion")
+            library("compose-ui", "androidx.compose.ui", "ui").versionRef("composeVersion")
+            library("compose-ui-preview", "androidx.compose.ui", "ui-tooling-preview").versionRef("composeVersion")
+            library("compose-material", "androidx.compose.material", "material").versionRef("composeVersion")
+            library("compose-animation", "androidx.compose.animation", "animation").versionRef("composeVersion")
+            bundle(
+                "compose",
+                listOf(
+                    "compose-ui",
+                    "compose-ui-util",
+                    "compose-ui-preview",
+                    "compose-material",
+                    "compose-animation"
+                )
+            )
+
+            version("exoPlayerVersion", "2.18.0")
+            library("exoplayer-mediasession", "com.google.android.exoplayer", "extension-mediasession").versionRef("exoPlayerVersion")
+            library("exoplayer-core", "com.google.android.exoplayer", "exoplayer-core").versionRef("exoPlayerVersion")
+            library("exoplayer-ui", "com.google.android.exoplayer", "exoplayer-ui").versionRef("exoPlayerVersion")
+            bundle(
+                "exoplayer",
+                listOf(
+                    "exoplayer-mediasession",
+                    "exoplayer-core",
+                    "exoplayer-ui"
+                )
+            )
+        }
+    }
+}
+
 rootProject.name = "Just Listen"
 include(":androidApp")
 include(":shared")
