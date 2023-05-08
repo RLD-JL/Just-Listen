46378a097457bf2bddf6f0d53c29552b2802a5ea
laurentiu <laurentzyu_danyel@yahoo.com>
2023-05-08 13:27:19 +0300

    google fix and top playlist fix

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,13 +40,13 @@ kapt {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
     defaultConfig {
         applicationId = "com.rld.justlisten.android"
         minSdk = 21
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 19
-        versionName = "1.0.7"
+        versionName = "1.0.6-fix"
         vectorDrawables {
             useSupportLibrary = true
         }
