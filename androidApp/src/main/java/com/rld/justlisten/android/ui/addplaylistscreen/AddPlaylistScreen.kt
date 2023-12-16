0f478f7e5f7f30176d139fb54af81d010bb605fa
RLD <rld.just.listen@gmail.com>
2023-12-16 20:38:09 +0200

    update ui to have back button on playlist

--- androidApp/src/main/java/com/rld/justlisten/android/ui/addplaylistscreen/AddPlaylistScreen.kt
+++ androidApp/src/main/java/com/rld/justlisten/android/ui/addplaylistscreen/AddPlaylistScreen.kt
@@ -1,9 +1,15 @@
 package com.rld.justlisten.android.ui.addplaylistscreen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,15 +26,24 @@ fun AddPlaylistScreen(
     addPlaylistState: AddPlaylistState,
     onAddPlaylistClicked: (String, String?) -> Unit,
     clickedToAddSongToPlaylist: (String, String?, List<String>) -> Unit,
-    events: Events
+    events: Events,
+    onBackButtonPressed: (Boolean) -> Unit
 ) {
     val openDialog = remember { mutableStateOf(false) }
-    LazyColumn(Modifier.fillMaxWidth()) {
-        item { AddPlaylistRow(openDialog) }
-        item { Divider(thickness = 2.dp) }
-        item { AddPlaylistDialog(openDialog, onAddPlaylistClicked) }
-        itemsIndexed(addPlaylistState.playlistsCreated) { _, playlist ->
-            PlaylistViewItem(playlist, clickedToAddSongToPlaylist, events)
+    Column(Modifier.fillMaxWidth()) {
+        IconButton(modifier = Modifier.size(48.dp), onClick = {onBackButtonPressed(true)  }) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = null,
+            )
+        }
+        LazyColumn(Modifier.fillMaxWidth()) {
+            item { AddPlaylistRow(openDialog) }
+            item { Divider(thickness = 2.dp) }
+            item { AddPlaylistDialog(openDialog, onAddPlaylistClicked) }
+            itemsIndexed(addPlaylistState.playlistsCreated) { _, playlist ->
+                PlaylistViewItem(playlist, clickedToAddSongToPlaylist, events)
+            }
         }
     }
 }
